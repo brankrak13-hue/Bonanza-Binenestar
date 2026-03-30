89a8a1598c21af388df328bed540058ae261ca3b
BrandBarsa <brankrak13@gmail.com>
2026-03-30 00:06:12 -0500

    Attempt 8: Coordination Fix - Update config.ts to use process.env with fallbacks for better Firebase App Hosting integration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-9297324962-e23f7",
-  "appId": "1:938375163616:web:3b8c55c0fb62fceeb5ea85",
-  "apiKey": "AIzaSyDkG1mbdGq_iuClDswHQIP76NdI4HKN-Ec",
-  "authDomain": "studio-9297324962-e23f7.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "938375163616"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-9297324962-e23f7",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:938375163616:web:3b8c55c0fb62fceeb5ea85",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDkG1mbdGq_iuClDswHQIP76NdI4HKN-Ec",
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-9297324962-e23f7.firebaseapp.com",
+  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "938375163616",
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-9297324962-e23f7.firebasestorage.app"
 };
